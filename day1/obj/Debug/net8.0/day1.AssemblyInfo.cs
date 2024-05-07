115b4b9e641f117e5198bcf802a4ee104ae6c9e3
Alif Laksono <alif7laksono@gmail.com>
2024-05-07 18:19:33 +0700

    update inheritance on projectC

--- day1/obj/Debug/net8.0/day1.AssemblyInfo.cs
+++ day1/obj/Debug/net8.0/day1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c5c2eda908ea5c679f6fb722e0386629a493c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+786f2bbce31ac09b0824feb808a1645b5d9c3970")]
 [assembly: System.Reflection.AssemblyProductAttribute("day1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
